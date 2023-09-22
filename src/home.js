3f6a3b116fadcc642e8f3d6e6101b16466644e66
Alif <alif.likes.coding@gmail.com>
2023-09-22 15:37:08 +0700

    Add partial main area

---
+++ src/home.js
@@ -0,0 +1,36 @@
+function createHome() {
+  const home = document.createElement('div');
+  home.classList.add('home');
+
+  const imageArea = document.createElement('div');
+  imageArea.classList.add('image-area');
+  home.appendChild(imageArea);
+  
+  const image1 = document.createElement('img');
+  image1.classList.add('image1');
+  image1.src = 'images/ice.png';
+  imageArea.appendChild(image1);
+  const image2 = document.createElement('img');
+  image2.classList.add('image2');
+  image2.src = 'images/vector.png';
+  imageArea.appendChild(image2);
+
+  const textArea = document.createElement('div');
+  textArea.classList.add('text-area');
+  home.appendChild(textArea);
+
+  const title = document.createElement('h1');
+  title.textContent = "I love IceCream"
+
+  textArea.appendChild(title);
+
+  return home;
+}
+
+function loadHome() {
+  const main = document.getElementById("main");
+  main.textContent = "";
+  main.appendChild(createHome());
+}
+
+export default loadHome;
--- src/website.js
+++ src/website.js
@@ -1,18 +1,124 @@
 import './style.css'
+import loadHome from "./home";
 
 function createHeader() {
   const header = document.createElement('header');
 
+  const logoArea = document.createElement('div');
+  logoArea.classList.add('logo-area');
   const logo = document.createElement('h1');
-  logo.innerText = "Medieval Chicken";
+  logo.innerText = "icecreamUscream";
   logo.classList.add('logo');
+  // <i class="fa-solid fa-ice-cream"></i>
+  const logoIcon = document.createElement('i');
+  logoIcon.classList.add('fa-solid', 'fa-ice-cream', 'logo-icon');
+  logoArea.appendChild(logoIcon);
+  logoArea.appendChild(logo);
+  header.appendChild(logoArea);
+  header.appendChild(createNav());
 
+  return header;
+}
+
+function createNav() {
+  const nav = document.createElement("nav");
+
+  const homeButton = document.createElement("button");
+  homeButton.classList.add("button-nav");
+  homeButton.classList.add("underline-animation");
+  homeButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(homeButton);
+    loadHome();
+  });
+  homeButton.innerHTML = "Home";
+  nav.appendChild(homeButton);
+
+  const menuButton = document.createElement("button");
+  menuButton.classList.add("button-nav");
+  menuButton.classList.add("underline-animation");
+  menuButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(menuButton);
+    loadMenu();
+  });
+  menuButton.innerHTML = "Menu";
+  nav.appendChild(menuButton);
+
+  const contactButton = document.createElement("button");
+  contactButton.classList.add("button-nav");
+  contactButton.classList.add("underline-animation");
+  contactButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(contactButton);
+    loadContact();
+  });
+  contactButton.innerHTML = "Contact";
+  nav.appendChild(contactButton);
+
+  return nav;
+}
+
+function setActiveButton(button) {
+  const buttons = document.querySelectorAll(".button-nav");
+
+  buttons.forEach((button) => {
+    if (button !== this) {
+      button.classList.remove("active");
+    }
+  });
+
+  button.classList.add("active");
+}
+
+function createMain() {
+  const main = document.createElement("main");
+  main.classList.add("main");
+  main.setAttribute("id", "main");
+  return main;
+}
+
+function createFooter() {
+  const footer = document.createElement("footer");
+
+  const gitHubArea = document.createElement("div");
+  const gitHubText = document.createElement("h1");
+  gitHubText.textContent = "aliflikescoding";
+  gitHubArea.appendChild(gitHubText);
+  // <i class="fa-brands fa-github"></i>
+  const gitHubLogo = document.createElement('i');
+  gitHubLogo.classList.add('git-hub-logo');
+  gitHubLogo.classList.add('fa-brands', 'fa-github');
+  gitHubArea.appendChild(gitHubLogo);
+  gitHubArea.classList.add('git-hub-area');
+  footer.appendChild(gitHubArea);
+
+  return footer;
+}
+
+function appendFontAwesome() {
+  const headElement = document.head;
+  const linkElement = document.createElement('link');
+
+  linkElement.rel = 'stylesheet';
+  linkElement.href = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css';
+  linkElement.integrity = 'sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==';
+  linkElement.crossOrigin = 'anonymous';
+  linkElement.referrerPolicy = 'no-referrer';
+
+  headElement.appendChild(linkElement);
 }
 
 function initializeWebsite() {
   const content = document.getElementById('content');
+  appendFontAwesome();
 
   content.appendChild(createHeader());
+  content.appendChild(createMain());
+  content.appendChild(createFooter());
 
-  
+  setActiveButton(document.querySelector(".button-nav"));
+  loadHome();
 }
+
+export default initializeWebsite;
